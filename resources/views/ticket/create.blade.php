99426783f374dd8a9d67b04bcc2e273b0e8ffad7
youin Salif <salifyouin@gmail.com>
2018-06-03 23:25:47 +0000

    amelioration du style creation et traitement de ticket,j'ai utilisé le midellware pasque j'ai pas vu le contenu

--- resources/views/ticket/create.blade.php
+++ resources/views/ticket/create.blade.php
@@ -8,30 +8,29 @@
                 <div class="card-header">Creation de nouveau ticket</div>
 
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-10 col-
-                        md-offset-1">
+                    
                           
                         {!! Form::open(['method'=>'POST','url'=>'ticket/enregistrer','class'=>'form-horizontal']) !!}
 
-                        <div class="btn-group {{$errors->has('message')? 'has-error':''}}">
+                        <div class="form-group {{$errors->has('message')? 'has-error':''}}">
                         {!! Form::Label('Message','Votre message') !!}
                         {!! Form::textarea('message',null,['class'=>'form-control','require'=>'require']) !!}
                         <small class="text-danger">{{$errors->first('message')}}</small>
                         </div>
-                        <div class="btn-group {{$errors->has('priorite_id')? 'has-error':''}}">
-                        {!! Form::Label('priorite_id','Input label') !!}
+                        <div class="form-group {{$errors->has('priorite_id')? 'has-error':''}}">
+                        {!! Form::Label('priorite_id','Priorité') !!}
                         {!! Form::select('priorite_id',$priorites,null,['class'=>'form-control','require'=>'require']) !!}
                         <small class="text-danger">{{$errors->first('priorite_id')}}</small>
                         </div>
 
-                        <div class="btn-group pull-right">
+                        <div class="btn-group">
                         {!! Form::reset("Annuler",['class'=>'btn btn-warning']) !!}
+                        </div>
+                        <div class="btn-group">
                         {!! Form::submit("enregistrer",['class'=>'btn btn-success']) !!}
                         </div>
                         {!! Form::close() !!}
-                        </div>
-                    </div>
+                        
                   
             </div>
         </div>
--- resources/views/traitement/new.blade.php
+++ resources/views/traitement/new.blade.php
@@ -7,16 +7,22 @@
             <div class="card">
                 <div class="card-header">Traitement du Ticket n° {{$ticket->id}} crée par {{$ticket->user->name}}, le {{$ticket->created_at}}</div>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-10 col-md-offset-1">
+                    
                         {!! Form::open(['method'=>'POST','url'=>'traitement/enregistrer','class'=>'form-horizontal']) !!}
-                        <div class="btn-group pull-right">
-                          {!! Form::reset("Annuler",['class'=>'btn btn-warning']) !!}
-                          {!! Form::submit("Enregistrer",['class'=>'btn btn-success']) !!}
+                        <div class="form-group {{$errors->has('description')? 'has-error':''}}">
+                        {!! Form::Label('description','Description') !!}
+                        {!! Form::textarea('description',null,['class'=>'form-control','require'=>'require']) !!}
+                        <small class="text-danger">{{$errors->first('description')}}</small>
+                        </div>
+                        <div class="btn-group">
+                        {!! Form::reset("Annuler",['class'=>'btn btn-warning']) !!}
+                        </div>
+                        <div class="btn-group">
+                        {!! Form::submit("enregistrer",['class'=>'btn btn-success']) !!}
+                       
                         </div>
                           {!! Form::close() !!}
-                        </div>
-                        </div>
+                       
                     </div> 
             </div>
         </div>
